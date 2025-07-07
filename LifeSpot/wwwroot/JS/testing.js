73de15b2a188cea2fd92b31b0a87f04f9aba5289
Name <email@example.ru>
2025-07-07 14:19:40 +0300

    Add functions

--- LifeSpot/wwwroot/JS/testing.js
+++ LifeSpot/wwwroot/JS/testing.js
@@ -1,5 +1,15 @@
-﻿// Получим коллекцию всех элементов страницы
-let elements = document.getElementsByTagName('*');
+﻿/*
+* Обработчик клика
+*
+* */
+const saveInput = function () {
+    // Вытащим значение текстового поля (как у нас уже делается при фильтрации)
+    let currentInput = document.getElementsByTagName('input')[0].value.toLowerCase();
 
-// Выведем результат в уведомление
-alert(`Количество элементов на странице:  ${elements.length}`);
+    // Покажем окно с прошлым и новым значением
+    alert('Последний ввод: ' + this.lastInput /* равноценно window.lastInput, так как мы работаем в контексте браузера */
+        + '\n' + 'Текущий ввод: ' + currentInput);
+
+    // Сохраним новое значение в контекст
+    this.lastInput = currentInput;
+}
